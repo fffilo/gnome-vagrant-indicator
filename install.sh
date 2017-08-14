3d4439e9e9b1e12f47a378f0d9a42cd130d769b6
Franjo Filo <fffilo666@gmail.com>
2017-08-14 10:54:29 +0200

    Makefile improvement

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+URL="https://github.com/fffilo"
+UUID="gnome-vagrant-indicator"
+BUILD="/tmp"
+
 # clear
-rm -rf /tmp/gnome-vagrant-indicator*
+rm -rf ${BUILD}/${UUID}*
 
 # download
-wget -O /tmp/gnome-vagrant-indicator.zip https://github.com/fffilo/gnome-vagrant-indicator/archive/master.zip
+wget -O ${BUILD}/${UUID}.zip ${URL}/${UUID}/archive/master.zip
 
 # unarchive
-unzip /tmp/gnome-vagrant-indicator.zip -d /tmp
+unzip ${BUILD}/${UUID}.zip -d ${BUILD}
 
 # install
-make --directory=/tmp/gnome-vagrant-indicator-master install
+make --directory=${BUILD}/${UUID}-master install
 
 # clear
-rm -rf /tmp/gnome-vagrant-indicator*
+rm -rf ${BUILD}/${UUID}*
