6df7310adca8b033a6f7ce1df4ea341f61aef57a
Franjo Filo <fffilo666@gmail.com>
2017-09-14 17:48:49 +0200

    Branch argument added to install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 URL="https://github.com/fffilo"
 UUID="gnome-vagrant-indicator"
+BRANCH="master"
 BUILD="/tmp"
+
+# first argument (branch) exists?
+[ -n "$1" ] && BRANCH="$1"
 
 # clear
 rm -rf ${BUILD}/${UUID}*
 
 # download
-wget -O ${BUILD}/${UUID}.zip ${URL}/${UUID}/archive/master.zip
+wget -O ${BUILD}/${UUID}.zip ${URL}/${UUID}/archive/${BRANCH}.zip
 
 # unarchive
 unzip ${BUILD}/${UUID}.zip -d ${BUILD}
